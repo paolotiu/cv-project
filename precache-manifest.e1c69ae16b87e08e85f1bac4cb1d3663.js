1122513d65ca261a5ccb9701337f13b797964e0f
paolotiu17 <paolotiu17@gmail.com>
2020-09-28 22:24:03 +0800

    Updates

---
+++ precache-manifest.e1c69ae16b87e08e85f1bac4cb1d3663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87b3e2dc2df36b27c424731ae20304b",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "71881cc0da016e47e38f",
+    "url": "/cv-project/static/js/2.41f4c939.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-project/static/js/2.41f4c939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b4c21b2cb7d97dcaa3",
+    "url": "/cv-project/static/js/main.fa06831b.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
